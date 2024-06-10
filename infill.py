be34932d9c840fa14b24981d2afa914eda55e965
Remi Kristoffer Aronsen <remi.kristoffer.aronsen@gmail.com>
2024-06-10 16:07:03 +0200

    updated requrements and made it possible to run infil on cpu

--- infill.py
+++ infill.py
@@ -13,24 +13,27 @@ def Infill(image, mask, prompt = ""):
     infill_needed = mask.any()
     if infill_needed:
         init_image = Image.fromarray(image.astype('uint8'))
-        mask_image = Image.fromarray(mask.astype('uint8')*255)
+        mask_image = Image.fromarray(mask.astype('uint8') * 255)
 
-        #resize the images to 512x512 to fit the model
+        # Resize the images to 512x512 to fit the model
         init_image = init_image.resize((512, 512))
         mask_image = mask_image.resize((512, 512))
 
+        # Determine device
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        dtype = torch.float16 if device == "cuda" else torch.float32
 
         pipe = StableDiffusionInpaintPipeline.from_pretrained(
-            "runwayml/stable-diffusion-inpainting", torch_dtype=torch.float16
+            "runwayml/stable-diffusion-inpainting", torch_dtype=dtype
         )
-        pipe = pipe.to("cuda")
+        pipe = pipe.to(device)
 
         infill_image = pipe(prompt=prompt, image=init_image, mask_image=mask_image).images[0]
-        
-        #resize the image back to the original size
+
+        # Resize the image back to the original size
         output_image = np.array(infill_image.resize((image.shape[1], image.shape[0])))
-        
-        #Replace part of image not infilled to keep original quality
+
+        # Replace part of image not infilled to keep original quality
         output_image[~mask] = image[~mask]
         
     else:
